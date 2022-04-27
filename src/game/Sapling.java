871867d794864e398712b9a71b9acfd7a8230a0e
msus0006 <msus0006@student.monash.edu>
2022-04-27 18:58:43 +0000

    sapling

---
+++ src/game/Sapling.java
@@ -0,0 +1,33 @@
+package game;
+
+import edu.monash.fit2099.engine.positions.Location;
+
+import java.util.Random;
+
+public class Sapling extends Tree{
+    private int saplingAge = 0;
+
+    public Sapling(){
+        super('t');
+    }
+
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        saplingAge += 1;
+        int spawnRateCoin = 10;
+
+        Random r = new Random();
+        int high = 101;
+        int low = 0;
+        int random = r.nextInt((high - low) + low);
+
+        if (random <= spawnRateCoin){
+            location.addItem(new Coin(20));
+        }
+
+        if (saplingAge == 10) {
+            location.setGround(new Mature());
+        }
+    }
+}
