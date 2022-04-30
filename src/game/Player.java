e1f9215a49af473cd1e54f38ee5fe2f5e1fe4235
Mikael Andrew <msus0006@student.monash.edu>
2022-05-01 02:42:06 +0800

    DONE

--- src/game/Player.java
+++ src/game/Player.java
@@ -6,14 +6,19 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.displays.Menu;
-import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import game.actions.ResetAction;
+import game.reset.Resettable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Class representing the Player.
  */
-public class Player extends Actor implements Resettable  {
+public class Player extends Actor implements Resettable {
 
 	private final Menu menu = new Menu();
 	private int marker = 0;
@@ -24,7 +29,7 @@ public class Player extends Actor implements Resettable  {
 	 *
 	 * @param name        Name to call the player in the UI
 	 * @param displayChar Character to represent the player in the UI
-	 * @param hitPoints   Player's starting number of hitpoints
+	 * @param hitPoints   Player's starting number of hit points
 	 */
 	public Player(String name, char displayChar, int hitPoints) {
 		super(name, displayChar, hitPoints);
@@ -36,6 +41,7 @@ public class Player extends Actor implements Resettable  {
 	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
 
 		decayPowerStarEffect(display);
+		actions.add(addJump(map));
 		// Handle multi-turn Actions
 		ResetAction reset = ResetAction.getInstance();
 
@@ -44,12 +50,24 @@ public class Player extends Actor implements Resettable  {
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
 
+
 		// check at every turn if certain Status should be removed.
+		if(this.hasCapability(Status.POWERSTAR)){
+			Location locate = map.locationOf(this);
+
+			if(map.at(locate.x(),locate.y()+1).getDisplayChar()=='#' || map.at(locate.x(),locate.y()+1).getDisplayChar()=='T' ){
+				Coin coin = new Coin(5);
+
+				map.at(locate.x()-1,locate.y()+1).addItem(coin);
+			}
+		}
 
 		// return/print the console menu
+		display.println(this.printHp() + "(" + map.locationOf(this).x() + ", " + map.locationOf(this).y() + ")");
 		return menu.showMenu(this, actions, display);
 
 	}
+
 
 	@Override
 	public char getDisplayChar(){
@@ -66,16 +84,17 @@ public class Player extends Actor implements Resettable  {
 		}
 		this.resetMaxHp(100); //heal to maximum
 	}
-	
+
 
 	public void addSuperMushroomEffect() {
 		this.addCapability(Status.SUPERMUSHROOM);
-		
+
 		this.increaseMaxHp(50);
 		this.setDisplayChar('M');
 
 		//TODO: jump
-		
+
+
 	}
 
 	private void removeSuperMushroomEffect() {
@@ -93,6 +112,11 @@ public class Player extends Actor implements Resettable  {
 	public void addPowerStarEffect() {
 		this.addCapability(Status.POWERSTAR);
 		powerStarEffectTicker = 10;
+		this.increaseMaxHp(200);
+
+
+
+
 	}
 
 	private void decayPowerStarEffect(Display display) {
@@ -105,6 +129,31 @@ public class Player extends Actor implements Resettable  {
 	}
 
 
-	
+	public List<Action> addJump(GameMap map){
+		ArrayList<Action> actions = new ArrayList<Action>();
+
+		int x = map.locationOf(this).x();
+		int y = map.locationOf(this).y();
+
+		List<Character> highGrounds = new ArrayList<Character>();
+		highGrounds.add('#');
+		highGrounds.add('+');
+		highGrounds.add('t');
+		highGrounds.add('T');
+
+		for (int i = -1; i < 2; i++){
+			for (int j = -1; j < 2; j++){
+				if (!(x + i == x && y + j == y)){
+					//System.out.println(highGrounds.contains(map.at((x + i), (y + j)).getDisplayChar()));
+					if (highGrounds.contains(map.at((x + i), (y + j)).getDisplayChar())){
+						Location highGround = map.at((x + i), (y + j));
+						actions.add(new JumpBehaviour(highGround.getGround(), highGround));
+					}
+				}
+			}
+		}
+		return actions;
+	}
+
 
 }
